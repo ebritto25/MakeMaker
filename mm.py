ff34b476585c47fcefbfdb0a80e9dc5dad5a4a0c
Eduardo B. Costa <ebritto25@gmail.com>
2018-03-22 11:47:49 -0300

    changed the file name

--- mm.py
+++ mm.py
@@ -6,14 +6,14 @@ parser = argparse.ArgumentParser(prog='MakeMaker',description='Create a MakeFile
 
 
 parser.add_argument('source_file',nargs=1,help='Source file to be compiled')
-parser.add_argument('-o','--output',nargs=1,default='exec',type=str,help='Defines the output name.')
+parser.add_argument('-o','--output',nargs=1,default=['exec'],type=str,help='Defines the output name.')
 
 args = vars(parser.parse_args())
 
 source = args['source_file'][0]
 output = args['output'][0]
 
-if(not source.endswith('.cpp') or not souce.endswith('.c')):
+if(not source.endswith('.cpp') or not source.endswith('.c')):
 	source += '.cpp'
 
 
@@ -24,7 +24,7 @@ with open('Makefile','w') as f:
 	f.write('compile:\n')
 
 	f.write("\t@echo 'Compiling'\n")
-	f.write("\tg++ "+source+" -o "+output+"\n")
+	f.write("\tg++ -std=c++11 "+source+" -o "+output+"\n")
 
 	f.write('test:\n')
 	f.write("\t@echo 'Testing'\n")
